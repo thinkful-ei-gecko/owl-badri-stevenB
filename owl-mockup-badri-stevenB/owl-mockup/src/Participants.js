6070e330950de20d7439abfe02f202904dbf61bc
Badri-narayan <badritulsiram@gmail.com>
2019-09-04 13:10:09 -0700

    some changes to Particpants.js

--- owl-mockup-badri-stevenB/owl-mockup/src/Participants.js
+++ owl-mockup-badri-stevenB/owl-mockup/src/Participants.js
@@ -3,8 +3,11 @@ import React from 'react';
 
 
 export default function(props){
-    const parts = props.xyz.map((val, i) => <li key={i}> <img src={val.avatar} alt={val.name} 
+    const inSessionArr = props.xyz.filter((val, i) => val.inSession === true).map((val, i) => <li key={i}> <img src={val.avatar} alt={val.name} 
 style={{borderRadius: "50%", height: "35px", border: "1px solid black"}}/> 
     {val.name} {val.inSession && <div className="green-dot" />}{!val.inSession && <div className="grey-dot" />}{val.onStage && 'on stage'}</li>)
-    return <ul>{ parts }</ul>
+    const notInSessionArr = props.xyz.filter((val, i) => val.inSession === false).map((val, i) => <li key={i}> <img src={val.avatar} alt={val.name} 
+    style={{borderRadius: "50%", height: "35px", border: "1px solid black"}}/> 
+        {val.name} {val.inSession && <div className="green-dot" />}{!val.inSession && <div className="grey-dot" />}{val.onStage && 'on stage'}</li>)
+    return <ul>{ inSessionArr } {notInSessionArr}</ul>
 }
