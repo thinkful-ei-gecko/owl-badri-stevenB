fbe590f1697619be7f227e4a86120652eb278f0d
Badri-narayan <badritulsiram@gmail.com>
2019-09-04 14:17:01 -0700

    minor css changes

--- owl-mockup-badri-stevenB/owl-mockup/src/Stage.js
+++ owl-mockup-badri-stevenB/owl-mockup/src/Stage.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function(props){
     console.log(props.abc);
     const theStage = props.abc.filter(val => val.onStage === true).map(val => {
-        return <div key={val.id} className='stagePeople'><img src={val.avatar} alt={val.name}/>{val.name}</div>;
+        return <div key={val.id} className='stagePeople'><img src={val.avatar} alt={val.name} style={{borderRadius: '50%', border: 'solid 1px black'}}/>{val.name}</div>;
     });
     return <section className='stageBox'>{ theStage }</section>;
 }
